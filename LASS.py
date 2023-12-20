d9a501e5c980aad274a6f0899bc339df988af22e
Влад Фомин <58701528+vladhog@users.noreply.github.com>
2023-12-20 21:19:24 +0300

    Update LASS.py

--- LASS.py
+++ LASS.py
@@ -9,19 +9,13 @@ api_url = "https://vladhog.ru/lass/api"
 
 
 def check_nsfw(file):
-    characters = string.ascii_letters
-    id = "".join([random.choice(characters) for i in range(8)])
-    requests.post(f"{api_url}/detection/nsfw", files={'file': file}, headers={"id": id})
-    return requests.get(f"{api_url}/detection/nsfw", headers={"id": id}).json()
-    # {'drawings': int, 'hentai': int, 'neutral': int, 'porn': int, 'sexy': int} all numbers are percentages
+    return requests.get(f"{api_url}/detection/nsfw", files={'file': file}).json()
+    # {'drawings': 1.5, 'hentai': 87.8, 'neutral': 1.5, 'porn': 7.7, 'sexy': 1.5} all numbers are p
+
 
 async def check_nsfw_async(file):
     data = FormData()
     data.add_field('file', file)
-    characters = string.ascii_letters
-    id = "".join([random.choice(characters) for i in range(8)])
-    async with aiohttp.ClientSession(headers={"id": id}) as session:
-        await session.post(f"{api_url}/detection/nsfw", data=data)
-        async with session.get(f"{api_url}/detection/nsfw") as a:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"{api_url}/detection/nsfw", data=data) as a:
             return await a.json()
-            # {'drawings': int, 'hentai': int, 'neutral': int, 'porn': int, 'sexy': int} all numbers are percentages
