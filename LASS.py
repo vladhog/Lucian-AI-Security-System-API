b21a57f21c7a925b1f171303e774d0da15fe6e4b
Влад Фомин <58701528+vladhog@users.noreply.github.com>
2023-12-24 01:46:19 +0300

    Update LASS.py
    AI link check out now

--- LASS.py
+++ LASS.py
@@ -8,14 +8,26 @@ from aiohttp import FormData
 api_url = "https://vladhog.ru/lass/api"
 
 
-def check_nsfw(file):
+def check_nsfw(file: BinaryIO) -> dict:
     return requests.get(f"{api_url}/detection/nsfw", files={'file': file}).json()
-    # {'drawings': 1.5, 'hentai': 87.8, 'neutral': 1.5, 'porn': 7.7, 'sexy': 1.5} all numbers are p
+    # {'drawings': 1.5, 'hentai': 87.8, 'neutral': 1.5, 'porn': 7.7, 'sexy': 1.5} all numbers are percentages
 
 
-async def check_nsfw_async(file):
+async def check_nsfw_async(file: BinaryIO) -> dict:
     data = FormData()
     data.add_field('file', file)
     async with aiohttp.ClientSession() as session:
         async with session.get(f"{api_url}/detection/nsfw", data=data) as a:
             return await a.json()
+            # {'response': safe, malware or blocked (blocked means site have anti bot protection or like that)}
+
+def check_url_content(url: str) -> dict:
+    return requests.get(f"{api_url}/detection/linkcheck/content", headers={"url": url}).json()
+    # {'response': safe, malware or blocked (blocked means site have anti bot protection or like that)}
+
+
+async def check_url_content_async(url: str) -> dict:
+    async with aiohttp.ClientSession(headers={"url": url}) as session:
+        async with session.get(f"{api_url}/detection/linkcheck/content") as a:
+            return await a.json()
+            # {'response': safe, malware or blocked (blocked means site have anti bot protection or like that)}
